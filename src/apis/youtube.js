94e26660f066e90dbcee37551b9d025d0de30dcd
oephi <r.sickle@protonmail.com>
2019-01-29 11:05:49 +1100

    video item and detail component done

--- src/apis/youtube.js
+++ src/apis/youtube.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const KEY = process.env.YOUTUBE_KEY;
+// const KEY = process.env.YOUTUBE_KEY;
+const KEY = "AIzaSyBoSFfB3pPi6g1fUqwS3Xd1UxbjORCyNPs";
 
 export default axios.create({
   baseURL: "https://www.googleapis.com/youtube/v3",
